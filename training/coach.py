264fc4a5a465299703c21eeb6bb5cffe5eb7a8ca
NilouAP <nalipou@clemson.edu>
2024-12-17 20:01:56 -0500

    coach file

--- training/coach.py
+++ training/coach.py
@@ -81,4 +81,47 @@ class Coach:
 		if self.opts.save_interval is None:
 			self.opts.save_interval = self.opts.max_steps
 
+	def train(self):
+		self.net.train()
+		while self.global_step < self.opts.max_steps:
+			for batch_idx, batch in enumerate(tqdm(self.train_dataloader)):
+				self.optimizer.zero_grad()
+				x, y = batch
+				x, y = x.to(self.device).float(), y.to(self.device).float()
+				y_hat, latent = self.net.forward(x, return_latents=True)
+				loss, loss_dict, id_logs = self.calc_loss(x, y, y_hat, latent)
+				loss.backward()
+				self.optimizer.step()
+
+				# Logging related
+				if self.global_step % self.opts.image_interval == 0 or (self.global_step < 1000 and self.global_step % 25 == 0):
+					self.parse_and_log_images(id_logs, x, y, y_hat, title='images/train/faces')
+				if self.global_step % self.opts.board_interval == 0:
+					self.print_metrics(loss_dict, prefix='train')
+					self.log_metrics(loss_dict, prefix='train')
+
+				# Log images of first batch to wandb
+				if self.opts.use_wandb and batch_idx == 0:
+					self.wb_logger.log_images_to_wandb(x, y, y_hat, id_logs, prefix="train", step=self.global_step, opts=self.opts)
+
+				# Validation related
+				val_loss_dict = None
+				if self.global_step % self.opts.val_interval == 0 or self.global_step == self.opts.max_steps:
+					val_loss_dict = self.validate()
+					if val_loss_dict and (self.best_val_loss is None or val_loss_dict['loss'] < self.best_val_loss):
+						self.best_val_loss = val_loss_dict['loss']
+						self.checkpoint_me(val_loss_dict, is_best=True)
+
+				if self.global_step % self.opts.save_interval == 0 or self.global_step == self.opts.max_steps:
+					if val_loss_dict is not None:
+						self.checkpoint_me(val_loss_dict, is_best=False)
+					else:
+						self.checkpoint_me(loss_dict, is_best=False)
+
+				if self.global_step == self.opts.max_steps:
+					print('OMG, finished training!')
+					break
+
+				self.global_step += 1
+
 
