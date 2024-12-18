07cae4ef1ef66279a37105ff28f1f0f7143b99d9
NilouAP <nalipou@clemson.edu>
2024-12-17 19:13:47 -0500

    get_network

--- criteria/lpips/networks.py
+++ criteria/lpips/networks.py
@@ -7,3 +7,13 @@ import torch.nn as nn
 from torchvision import models
 
 from criteria.lpips.utils import normalize_activation
+
+def get_network(net_type: str):
+    if net_type == 'alex':
+        return AlexNet()
+    elif net_type == 'squeeze':
+        return SqueezeNet()
+    elif net_type == 'vgg':
+        return VGG16()
+    else:
+        raise NotImplementedError('choose net_type from [alex, squeeze, vgg].')
