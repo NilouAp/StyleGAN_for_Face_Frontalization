e48e70bb5c3404bd7a80e604ab599da3a1f63681
NilouAP <nalipou@clemson.edu>
2024-12-17 19:05:40 -0500

    moco  loss

--- criteria/moco_loss.py
+++ criteria/moco_loss.py
@@ -2,3 +2,11 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from configs.paths_config import model_paths
+class MocoLoss(nn.Module):
+
+    def __init__(self):
+        super(MocoLoss, self).__init__()
+        print("Loading MOCO model from path: {}".format(model_paths["moco"]))
+        self.model = self.__load_model()
+        self.model.cuda()
+        self.model.eval()
